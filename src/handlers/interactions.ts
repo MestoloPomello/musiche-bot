8bf4f60a05cb464bde2f8ceeb6ba1af8d5481e38
MestoloPomello <stefanonava99@gmail.com>
2026-03-16 21:34:42 +0100

    feat: replyOrFollowUp

---
+++ src/handlers/interactions.ts
@@ -0,0 +1,19 @@
+import { CommandInteraction } from "discord.js";
+
+/**
+ * Replies to an interaction if it hasn't been replied to or deferred yet, otherwise sends a follow-up message.
+ */
+export async function replyOrFollowUp(
+	interaction: CommandInteraction,
+	content: object 
+): Promise<void> {
+	try {
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp(content);
+		} else {
+			await interaction.reply(content);
+		}
+	} catch (error: any) {
+		console.trace("replyOrFollowUp error:", error);
+	}
+}
