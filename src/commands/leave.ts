74dced51b109565d7cda96d569f457bf23af42a9
MestoloPomello <stefanonava99@gmail.com>
2026-03-26 23:57:39 +0100

    feat: leave slash cmd handled like btn

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -4,6 +4,7 @@ import {
 } from "discord.js";
 import { getVoiceConnection } from "@discordjs/voice";
 import { replyOrFollowUp } from "../handlers/interactions";
+import { destroyGuildInstance } from "../handlers/connections";
 
 export const data = new SlashCommandBuilder()
 	.setName("leave")
@@ -12,10 +13,9 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction: CommandInteraction) {
 	try {
 		if (!interaction.guildId) throw "questo comando non funziona in privato.";
+		if (!getVoiceConnection(interaction.guildId)) throw "non sono in un canale vocale.";
 
-		const voiceConnection = getVoiceConnection(interaction.guildId);
-		if (!voiceConnection) throw "non sono in un canale vocale.";
-		voiceConnection.destroy();
+		destroyGuildInstance(interaction.guildId);
 
 		replyOrFollowUp(interaction, {
 			content: `Ho abbandonato il canale vocale.`
